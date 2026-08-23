5bf12c65aebceee13608023bc812defb19a482d9
Matt Mercure <matt.mercure5896@gmail.com>
2026-08-23 09:50:41 -0300

    Added event listeners to each div in he grid and have them changed to blue once the mouse hovers over them.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,6 +4,8 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const box = document.createElement('div');
         container.appendChild(box);
+        box.addEventListener('mouseenter', () => {
+            box.style.backgroundColor = 'blue';
+        });
     }
 }
-
